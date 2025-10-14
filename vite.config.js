abce4e496cba0d966950bcb79bbe6a6cee26cf40
xueguys <yangyuehua_1015@qq.com>
2025-10-14 22:30:33 +0800

    k

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // When deploying to GitHub Pages under username.github.io/repo
-  base: '/gxfcfxxccxv/',
+  // When deploying to custom domain
+  base: 'https://kananosa.github.io/gxfcfxxccxv/',
   plugins: [react()],
 })
