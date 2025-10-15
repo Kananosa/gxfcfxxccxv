88e36419c0e09ef227d5f9a94d5e4e770d2f6866
xueguys <yangyuehua_1015@qq.com>
2025-10-15 12:13:08 +0800

    u

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // When deploying to custom domain
-  base: 'https://kananosa.github.io/gxfcfxxccxv/',
+  // When deploying to Cloudflare Pages
+  base: '/',
   plugins: [react()],
 })
